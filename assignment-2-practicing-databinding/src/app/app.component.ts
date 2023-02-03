2ba0e5b8272b1c0f63dc5f788a82501ff19c6789
Samuel Banya <sbanya@fastmail.com>
2023-02-03 08:59:55 -0600

    Adding changes to fix previous import issue, and was able to get assignment working

--- assignment-2-practicing-databinding/src/app/app.component.ts
+++ assignment-2-practicing-databinding/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-2-practicing-databinding';
+
+  username = '';
+
+  resetString() {
+    this.username = '';
+  }
 }
--- assignment-2-practicing-databinding/src/app/app.module.ts
+++ assignment-2-practicing-databinding/src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+// NOTE: This is required for '[(ngModel)]' based two-way-data-binding to actually work:
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
